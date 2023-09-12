a836531e43524a8db26d796996eb921e082a142f
mehedihanik <mehedih60563@gmailcom>
2023-09-12 17:03:09 +0600

    commit

--- problems.js
+++ problems.js
@@ -1,6 +1,25 @@
+// Problem1
 function anaToVori(ana){
     var vori = ana/16;
     return vori;
 }
 
 console.log(anaToVori(64))
+
+// Problem2
+
+function pandaCost(singara, samusa, jilapi){
+    if (typeof singara =='number' && typeof samusa == 'number'&& typeof jilapi =='number') {
+       var totalSingara = singara*7;
+       var totalSamusa = samusa*10;
+       var totaljilapi = jilapi*15;
+       var totalCost = totalSamusa+totalSingara+totaljilapi;
+       return totalCost; 
+    }
+    else {
+        console.log("invalid input")
+    }
+
+}
+
+console.log(pandaCost(1, 2, 2))
